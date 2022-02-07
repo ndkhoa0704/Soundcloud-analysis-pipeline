93afe8c69ab76ba9c84b5845383f886ac8f2d2e0
koanguyn <ndkhoa0704@gmail.com>
2022-02-07 23:50:20 +0700

    add crawl user

---
+++ main.py
@@ -0,0 +1,6 @@
+from pipeline.getdata import Crawler
+
+if __name__ == '__main__':
+    crawler = Crawler(1, 100, 20)
+    crawler.get_data()
+    crawler.terminate()
--- pipeline/getdata.py
+++ pipeline/getdata.py
@@ -7,39 +7,64 @@ import json
 import random
 import pandas as pd
 
-class soundcloud_crawler:
-    def __init__(self, executable_path=None):
-        # Use both api and html parser method to crawl data 
+
+class Crawler:
+    def __init__(self, userid_min, userid_max, no_users, executable_path=None, data_path='../data'):
+        # Use both api and html parser method to crawl data
+
+        # Init variables
+        self._userid_max = userid_max
+        self._userid_min = userid_min
+        self._no_users = no_users
+        self._data_path = data_path
+        self._user_ids = random.sample(
+            range(self._userid_min, self._userid_max + 1), self._no_users)
+
+        # Create driver
         option = Options()
-        service = Service(executable_path)
-        option.add_argument('headless', 'disable-gpu')
+        option.add_argument('headless')
+        option.add_argument('disable-gpu')
+        self._driver = None
         if executable_path:
-            self.driver = Chrome(executable_path=executable_path, options=option)
+            self._driver = Chrome(service=Service(
+                executable_path), options=option)
         else:
-            self.driver = Chrome(service=service, options=option)
-        
-    
+            self._driver = Chrome(options=option)
+
+        # Get client id
+        self._client_id = self._get_client_id()
+
     def _get_client_id(self):
         # Use selenium to get client id
         # Get client id of the logged-in user in the browser
-        self.driver.get('https://soundcloud.com')
-        log = self.driver.get_log('browser')
-        for i in log:
-            client_id = re.search(r'client_id=(.*?)\&', i)
+        self._driver.get('https://soundcloud.com')
+        log = self._driver.get_log('browser')
+        client_id = re.search(r"client_id=(.*?)\&", log[0]['message']).group(1)
         return client_id
-    
-    def get_data(self, estimated_no_users, min_user_id, max_user_id, path='../data'):
-        client_id = self._get_client_id()
-        user_ids = random.sample(range(min_user_id,max_user_id + 1),estimated_no_users)
+
+    def _get_user_info(self, path):
+        # Get user data and store in user.csv
+        jsondata = []
+        for user_id in self._user_ids:
+            response = None
+            response = requests.get(
+                f'https://api-v2.soundcloud.com/users/{user_id}?client_id={self._client_id}')
+            if response.ok:
+                jsondata.append(response.json())
+        data = pd.DataFrame(jsondata)
+        data.to_csv(path + '/user.csv',index=False)
+
+    def _get_track_data(self, path):
         data = pd.DataFrame('')
-        for user_id in user_ids:
-            user_ids.append(random.randint(min_user_id, max_user_id))
-            response = requests.get(f"https://api-v2.soundcloud.com/users/{user_id}/tracks?client_id={client_id}")
+        for user_id in self._user_ids:
+            response = requests.get(
+                f"https://api-v2.soundcloud.com/users/{user_id}/tracks?client_id={self._client_id}")
             if response.ok:
                 response.json()['collection']
 
+    def get_data(self,  path='./data'):
+        self._get_user_info(path)
+        self._terminate()
 
-
-
-
-        
+    def _terminate(self):
+        self._driver.close()
