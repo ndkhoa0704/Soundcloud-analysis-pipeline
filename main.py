47faec5636c3034c11ba79eef5d271fd6c849876
koanguyn <ndkhoa0704@gmail.com>
2022-02-15 07:51:56 +0700

    add code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     crawler = SoundcloudCrawler(
         userid_min=1,
         userid_max=99999999,
-        no_users=5000,
+        no_users=100,
         no_tracks_liked=1000,
         no_tracks_created=1000,
         no_playlists_liked=1000,
